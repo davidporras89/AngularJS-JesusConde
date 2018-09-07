4c7621c0efac8c5fcfda7654b1a03470e3f73a4f
davidporras89 <davidporras89@gmail.com>
2018-09-07 16:18:06 -0500

    Ejercicio Tareas Jesus conde Terminado

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,8 +16,13 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'ui.sortable'
-  ])
+    'ui.sortable',
+    'LocalStorageModule',
+
+  ]).config(['localStorageServiceProvider',function(localStorageServiceProvider){
+    localStorageServiceProvider.setPrefix('ls');
+  }])
+
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,8 +8,12 @@
  * Controller of the tareaApp
  */
 angular.module('tareaApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.tareas = ['Item 1','Item 2','Item 3'];
+  .controller('MainCtrl', function ($scope, localStorageService) {
+    var tareasEnAlmacen = localStorageService.get('tareas');
+    $scope.tareas = tareasEnAlmacen && tareasEnAlmacen.split('\n') || [];
+    $scope.$watch('tareas',function(){
+      localStorageService.add('tareas', $scope.tareas.join('\n'));
+    }, true);
     $scope.addTarea = function(){
       $scope.tareas.push($scope.tarea);
       $scope.tarea='';
